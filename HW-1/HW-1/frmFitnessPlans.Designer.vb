e42fedc409edf0efa5ec83f05ac85033c1deb8b7
smead001 <smead001@fiu.edu>
2022-02-09 01:38:13 -0500

    HW-1 version 1

---
+++ HW-1/HW-1/frmFitnessPlans.Designer.vb
@@ -0,0 +1,439 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFitnessPlans
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.grpPlanChoices = New System.Windows.Forms.GroupBox()
+        Me.Label22 = New System.Windows.Forms.Label()
+        Me.radBasicInd = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton7 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton8 = New System.Windows.Forms.RadioButton()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.chkTrainer = New System.Windows.Forms.CheckBox()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.grpTrainer = New System.Windows.Forms.GroupBox()
+        Me.grpPlanChoices.SuspendLayout()
+        Me.grpTrainer.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(101, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(429, 61)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "WELCOME TO OUR FITNESS CENTER!"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(25, 109)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(168, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "please choose a Membership plan"
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Silver
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(29, 131)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 48)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Fitness Membership plan"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Silver
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(115, 131)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(238, 48)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Description"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.Silver
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(347, 131)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(101, 48)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Individual Level"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.Silver
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(444, 131)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(102, 48)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Family Level"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.Color.Transparent
+        Me.Label7.Location = New System.Drawing.Point(6, 2)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(87, 34)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Basic"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label8
+        '
+        Me.Label8.BackColor = System.Drawing.Color.Transparent
+        Me.Label8.Location = New System.Drawing.Point(6, 48)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(87, 34)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Basic Plus"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label9
+        '
+        Me.Label9.BackColor = System.Drawing.Color.Transparent
+        Me.Label9.Location = New System.Drawing.Point(6, 96)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(87, 34)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "Preferred"
+        Me.Label9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label10
+        '
+        Me.Label10.BackColor = System.Drawing.Color.Transparent
+        Me.Label10.Location = New System.Drawing.Point(90, 2)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(235, 34)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "Use of gym facilities with basic equipment only"
+        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'grpPlanChoices
+        '
+        Me.grpPlanChoices.BackColor = System.Drawing.Color.White
+        Me.grpPlanChoices.Controls.Add(Me.Label12)
+        Me.grpPlanChoices.Controls.Add(Me.Label11)
+        Me.grpPlanChoices.Controls.Add(Me.Label13)
+        Me.grpPlanChoices.Controls.Add(Me.Label22)
+        Me.grpPlanChoices.Controls.Add(Me.RadioButton8)
+        Me.grpPlanChoices.Controls.Add(Me.Label9)
+        Me.grpPlanChoices.Controls.Add(Me.Label10)
+        Me.grpPlanChoices.Controls.Add(Me.Label8)
+        Me.grpPlanChoices.Controls.Add(Me.Label7)
+        Me.grpPlanChoices.Controls.Add(Me.RadioButton7)
+        Me.grpPlanChoices.Controls.Add(Me.RadioButton4)
+        Me.grpPlanChoices.Controls.Add(Me.RadioButton2)
+        Me.grpPlanChoices.Controls.Add(Me.RadioButton6)
+        Me.grpPlanChoices.Controls.Add(Me.RadioButton5)
+        Me.grpPlanChoices.Controls.Add(Me.RadioButton3)
+        Me.grpPlanChoices.Controls.Add(Me.radBasicInd)
+        Me.grpPlanChoices.Location = New System.Drawing.Point(28, 182)
+        Me.grpPlanChoices.Name = "grpPlanChoices"
+        Me.grpPlanChoices.Size = New System.Drawing.Size(518, 194)
+        Me.grpPlanChoices.TabIndex = 18
+        Me.grpPlanChoices.TabStop = False
+        '
+        'Label22
+        '
+        Me.Label22.BackColor = System.Drawing.Color.Transparent
+        Me.Label22.Location = New System.Drawing.Point(6, 148)
+        Me.Label22.Name = "Label22"
+        Me.Label22.Size = New System.Drawing.Size(87, 34)
+        Me.Label22.TabIndex = 19
+        Me.Label22.Text = "Deluxe"
+        Me.Label22.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'radBasicInd
+        '
+        Me.radBasicInd.AutoSize = True
+        Me.radBasicInd.BackColor = System.Drawing.Color.Transparent
+        Me.radBasicInd.Location = New System.Drawing.Point(344, 11)
+        Me.radBasicInd.Name = "radBasicInd"
+        Me.radBasicInd.Size = New System.Drawing.Size(58, 17)
+        Me.radBasicInd.TabIndex = 0
+        Me.radBasicInd.TabStop = True
+        Me.radBasicInd.Text = "$14.95"
+        Me.radBasicInd.UseVisualStyleBackColor = False
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.BackColor = System.Drawing.Color.Transparent
+        Me.RadioButton3.Location = New System.Drawing.Point(344, 57)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(58, 17)
+        Me.RadioButton3.TabIndex = 1
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "$26.95"
+        Me.RadioButton3.UseVisualStyleBackColor = False
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.BackColor = System.Drawing.Color.Transparent
+        Me.RadioButton5.Location = New System.Drawing.Point(344, 105)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(58, 17)
+        Me.RadioButton5.TabIndex = 2
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "$26.95"
+        Me.RadioButton5.UseVisualStyleBackColor = False
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.BackColor = System.Drawing.Color.Transparent
+        Me.RadioButton6.Location = New System.Drawing.Point(344, 157)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(58, 17)
+        Me.RadioButton6.TabIndex = 3
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "$45.95"
+        Me.RadioButton6.UseVisualStyleBackColor = False
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.BackColor = System.Drawing.Color.Transparent
+        Me.RadioButton2.Location = New System.Drawing.Point(444, 11)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(58, 17)
+        Me.RadioButton2.TabIndex = 22
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "$24.95"
+        Me.RadioButton2.UseVisualStyleBackColor = False
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.BackColor = System.Drawing.Color.Transparent
+        Me.RadioButton4.Location = New System.Drawing.Point(444, 57)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(58, 17)
+        Me.RadioButton4.TabIndex = 23
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "$23.00"
+        Me.RadioButton4.UseVisualStyleBackColor = False
+        '
+        'RadioButton7
+        '
+        Me.RadioButton7.AutoSize = True
+        Me.RadioButton7.BackColor = System.Drawing.Color.Transparent
+        Me.RadioButton7.Location = New System.Drawing.Point(444, 105)
+        Me.RadioButton7.Name = "RadioButton7"
+        Me.RadioButton7.Size = New System.Drawing.Size(58, 17)
+        Me.RadioButton7.TabIndex = 24
+        Me.RadioButton7.TabStop = True
+        Me.RadioButton7.Text = "$48.00"
+        Me.RadioButton7.UseVisualStyleBackColor = False
+        '
+        'RadioButton8
+        '
+        Me.RadioButton8.AutoSize = True
+        Me.RadioButton8.BackColor = System.Drawing.Color.Transparent
+        Me.RadioButton8.Location = New System.Drawing.Point(444, 157)
+        Me.RadioButton8.Name = "RadioButton8"
+        Me.RadioButton8.Size = New System.Drawing.Size(58, 17)
+        Me.RadioButton8.TabIndex = 25
+        Me.RadioButton8.TabStop = True
+        Me.RadioButton8.Text = "$60.00"
+        Me.RadioButton8.UseVisualStyleBackColor = False
+        '
+        'Label11
+        '
+        Me.Label11.BackColor = System.Drawing.Color.Transparent
+        Me.Label11.Location = New System.Drawing.Point(93, 48)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(232, 34)
+        Me.Label11.TabIndex = 26
+        Me.Label11.Text = "Use of gym facilities and locker room with basic equipment only"
+        Me.Label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label12
+        '
+        Me.Label12.BackColor = System.Drawing.Color.Transparent
+        Me.Label12.Location = New System.Drawing.Point(99, 148)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(226, 34)
+        Me.Label12.TabIndex = 27
+        Me.Label12.Text = "Use of gym facilities and locker room with advanced equipment, plus free refreshm" &
+    "ents at the smoothie bar"
+        Me.Label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label13
+        '
+        Me.Label13.BackColor = System.Drawing.Color.Transparent
+        Me.Label13.Location = New System.Drawing.Point(96, 96)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(229, 34)
+        Me.Label13.TabIndex = 28
+        Me.Label13.Text = "Use of gym facilities and locker room with advanced equipment"
+        Me.Label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label14
+        '
+        Me.Label14.BackColor = System.Drawing.Color.Transparent
+        Me.Label14.Location = New System.Drawing.Point(6, 16)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(83, 80)
+        Me.Label14.TabIndex = 29
+        Me.Label14.Text = "Optional add-on for personal trainer sessions"
+        '
+        'Label15
+        '
+        Me.Label15.BackColor = System.Drawing.Color.Transparent
+        Me.Label15.Location = New System.Drawing.Point(99, 10)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(226, 48)
+        Me.Label15.TabIndex = 30
+        Me.Label15.Text = "15 minutes with a personal trainer during each visit"
+        Me.Label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'chkTrainer
+        '
+        Me.chkTrainer.AutoSize = True
+        Me.chkTrainer.Location = New System.Drawing.Point(351, 41)
+        Me.chkTrainer.Name = "chkTrainer"
+        Me.chkTrainer.Size = New System.Drawing.Size(151, 17)
+        Me.chkTrainer.TabIndex = 31
+        Me.chkTrainer.Text = "Optional Personal Trainer?"
+        Me.chkTrainer.UseVisualStyleBackColor = True
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.BackColor = System.Drawing.Color.Transparent
+        Me.Label16.Location = New System.Drawing.Point(452, 16)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(40, 13)
+        Me.Label16.TabIndex = 19
+        Me.Label16.Text = "$25.00"
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.BackColor = System.Drawing.Color.Transparent
+        Me.Label17.Location = New System.Drawing.Point(348, 16)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(40, 13)
+        Me.Label17.TabIndex = 20
+        Me.Label17.Text = "$20.00"
+        '
+        'grpTrainer
+        '
+        Me.grpTrainer.BackColor = System.Drawing.Color.White
+        Me.grpTrainer.Controls.Add(Me.Label16)
+        Me.grpTrainer.Controls.Add(Me.Label14)
+        Me.grpTrainer.Controls.Add(Me.Label17)
+        Me.grpTrainer.Controls.Add(Me.Label15)
+        Me.grpTrainer.Controls.Add(Me.chkTrainer)
+        Me.grpTrainer.Location = New System.Drawing.Point(28, 382)
+        Me.grpTrainer.Name = "grpTrainer"
+        Me.grpTrainer.Size = New System.Drawing.Size(518, 72)
+        Me.grpTrainer.TabIndex = 19
+        Me.grpTrainer.TabStop = False
+        '
+        'frmFitnessPlans
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(773, 582)
+        Me.Controls.Add(Me.grpTrainer)
+        Me.Controls.Add(Me.grpPlanChoices)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmFitnessPlans"
+        Me.Text = "Fitness Plan Information"
+        Me.grpPlanChoices.ResumeLayout(False)
+        Me.grpPlanChoices.PerformLayout()
+        Me.grpTrainer.ResumeLayout(False)
+        Me.grpTrainer.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents grpPlanChoices As GroupBox
+    Friend WithEvents Label22 As Label
+    Friend WithEvents radBasicInd As RadioButton
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton8 As RadioButton
+    Friend WithEvents RadioButton7 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents chkTrainer As CheckBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents grpTrainer As GroupBox
+End Class
---
+++ HW-1/HW-1/frmFitnessPlans.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmFitnessPlans
+    Private Sub Label16_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label22_Click(sender As Object, e As EventArgs) Handles Label22.Click
+
+    End Sub
+
+    Private Sub Label10_Click(sender As Object, e As EventArgs) Handles Label10.Click
+
+    End Sub
+End Class
